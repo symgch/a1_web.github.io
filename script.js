3beb0d67e8faf49231675b20663fd527552cf6d6
Shuhei Yamaguchi <shuhei@SY-MacbookPro.lan>
2022-10-01 17:04:23 -0400

    Finished Gallery, Added Color Changing Button

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeColor() {
+	let color = document.body.style.color;
+	if (color === 'white') {
+		document.body.style.color = 'pink';
+	} else {
+		document.body.style.color = 'white';
+	}
+}
